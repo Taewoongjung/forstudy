a42851358c082ab4ef8443620c05cb1897bdf3c0
tae <aipooh8882@naver.com>
2021-01-10 23:43:43 +0900

    로그인

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET /user 라우터
+router.post('/', (req, res) => {
+    res.cookie('myCookie', process.env.COOKIE_SECRET);
+//    console.log(req.sessionID);
+
+    res.send('Hello '+ req.body.name_user);
+});
+
+module.exports = router;
