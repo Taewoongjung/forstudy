acc6531c455d1067f7a4d9ad95d8152bbeceb207
tae <aipooh8882@naver.com>
2021-01-14 02:39:38 +0900

    trying to provide cookies when it is remembered

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 // GET /user 라우터
-router.get('/user', (req, res) => {
-    res.cookie('myCookie', process.env.COOKIE_SECRET);
-//    console.log(req.sessionID);
-});
+router.get('/', (req, res) => {
+    console.log("id : "+ req.body.id);
+    if(req.body.rememberId === "checked"){
+      console.log("아이디 저장 체크");
+
+      res.cookie('Cookie', process.env.COOKIE_SECRET);
+
+      console.log(req.cookies);
+    }
+    res.render('index2', { me : req.query.name_user});
+  });
+
+// router.get('/', (req, res) => {
+//     req.clearCookie('myCookie', {path: '/'});
+// });
 
 module.exports = router;
